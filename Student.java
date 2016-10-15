a0f79282cf08b6365a7c5ae1d4402c0522218b42
Seth Kinel <sethak@hawaii.edu>
2016-10-15 02:02:27 +0000

    Add Student.java to project.

---
+++ Student.java
@@ -0,0 +1,16 @@
+public class Student{
+  int age;
+  float gpa;
+  String firstName;
+  String lastName;
+  String major;
+
+  public student(){
+
+  }
+  public Student(String firstName, String lastName, int age){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.age = age;
+  }
+}
